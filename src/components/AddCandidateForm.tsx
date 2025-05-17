8715765882ded0c377e0500351675da5e5964a41
[dyad] <git@dyad.sh>
2025-05-17 11:27:11 +0700

    [dyad] Memperbaiki error Select.Item di AddCandidateForm.tsx - wrote 1 file(s)

--- src/components/AddCandidateForm.tsx
+++ src/components/AddCandidateForm.tsx
@@ -147,9 +147,9 @@ const AddCandidateForm = ({ onCandidateAdded, refreshPositionsTrigger }: AddCand
                   </FormControl>
                   <SelectContent>
                     {loadingPositions ? (
-                      <SelectItem disabled value="">Memuat posisi...</SelectItem>
+                      <SelectItem disabled>Memuat posisi...</SelectItem>
                     ) : positions.length === 0 ? (
-                       <SelectItem disabled value="">Belum ada posisi</SelectItem>
+                       <SelectItem disabled>Belum ada posisi</SelectItem>
                     ) : (
                       positions.map((position) => (
                         <SelectItem key={position.id} value={position.id}>
