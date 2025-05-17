0c8fe41be3a1982a9a1c3db696668f78abaf7922
[dyad] <git@dyad.sh>
2025-05-17 07:28:43 +0700

    [dyad] Menambahkan fitur pengelolaan wawancara di modul Rekrutmen - wrote 2 file(s)

--- src/pages/RecruitmentPage.tsx
+++ src/pages/RecruitmentPage.tsx
@@ -2,23 +2,28 @@
 
 import React from 'react';
 import PositionManager from '@/components/PositionManager';
-import CandidateManager from '@/components/CandidateManager'; // Import the new component
+import CandidateManager from '@/components/CandidateManager';
+import InterviewManager from '@/components/InterviewManager'; // Import the new component
 
 const RecruitmentPage = () => {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-6 text-center">Modul Rekrutmen Karyawan</h1>
       <p className="text-center text-gray-600 mb-8">
-        Kelola posisi yang dibutuhkan dan data kandidat di sini.
+        Kelola posisi yang dibutuhkan, data kandidat, proses wawancara, dan keputusan di sini.
       </p>
 
       <PositionManager />
 
-      <div className="mt-10"> {/* Add some margin between sections */}
-        <CandidateManager /> {/* Add the CandidateManager component */}
+      <div className="mt-10">
+        <CandidateManager />
       </div>
 
-      {/* TODO: Tambahkan komponen untuk mengelola Wawancara dan Keputusan */}
+      <div className="mt-10"> {/* Add some margin */}
+        <InterviewManager /> {/* Add the InterviewManager component */}
+      </div>
+
+      {/* TODO: Tambahkan komponen untuk mengelola Keputusan */}
     </div>
   );
 };
