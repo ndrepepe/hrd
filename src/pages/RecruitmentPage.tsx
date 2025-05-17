7fb504987d324b4b8f0f542c43b5bd7efaed11c4
[dyad] <git@dyad.sh>
2025-05-17 07:26:02 +0700

    [dyad] Menambahkan fitur pengelolaan posisi di modul Rekrutmen - wrote 2 file(s)

--- src/pages/RecruitmentPage.tsx
+++ src/pages/RecruitmentPage.tsx
@@ -1,15 +1,19 @@
 "use client";
 
 import React from 'react';
+import PositionManager from '@/components/PositionManager'; // Import the new component
 
 const RecruitmentPage = () => {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-6 text-center">Modul Rekrutmen Karyawan</h1>
-      <p className="text-center text-gray-600">
-        Halaman ini akan berisi fitur untuk mengelola posisi, kandidat, proses wawancara, dan keputusan rekrutmen.
+      <p className="text-center text-gray-600 mb-8">
+        Kelola posisi yang dibutuhkan dan data kandidat di sini.
       </p>
-      {/* TODO: Tambahkan komponen untuk mengelola Posisi, Kandidat, Wawancara, dan Keputusan */}
+      
+      <PositionManager /> {/* Add the PositionManager component */}
+
+      {/* TODO: Tambahkan komponen untuk mengelola Kandidat, Wawancara, dan Keputusan */}
     </div>
   );
 };
