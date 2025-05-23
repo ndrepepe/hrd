a1279d7f163e6e06d1facf0f86f203967db8aec2
[dyad] <git@dyad.sh>
2025-05-23 20:05:43 +0700

    [dyad] Memperbaiki error React.Children.only pada DecisionManager dan InterviewManager - wrote 2 file(s)

--- src/components/DecisionManager.tsx
+++ src/components/DecisionManager.tsx
@@ -220,12 +220,15 @@ const DecisionManager = () => {
                                 !field.value && "text-muted-foreground"
                             )}
                             >
-                            {field.value ? (
+                            {/* WRAP CONTENT IN SPAN */}
+                            <span className="flex justify-between items-center w-full">
+                                {field.value ? (
                                 format(field.value, "PPP")
-                            ) : (
+                                ) : (
                                 <span>Pilih tanggal</span>
-                            )}
-                            <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                )}
+                                <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                            </span>
                             </Button>
                         </FormControl>
                         </PopoverTrigger>
